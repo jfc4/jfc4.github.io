eb32702e51d36cde576ebbe06a93e6c865e0d74d
Jessica Carter <jessica.carter@quadrants.co>
2020-02-09 23:03:52 -0500

    Updates

---
+++ precache-manifest.536147d972f83185c2483ff6a6b51951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae7ac010a310f6237fe3f67ca95ea03",
+    "url": "/jfc4.github.io/index.html"
+  },
+  {
+    "revision": "aa98456122814fc73e50",
+    "url": "/jfc4.github.io/static/css/main.828929ca.chunk.css"
+  },
+  {
+    "revision": "adc4aa6307d2753fe9ee",
+    "url": "/jfc4.github.io/static/js/2.56600d9d.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/jfc4.github.io/static/js/2.56600d9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa98456122814fc73e50",
+    "url": "/jfc4.github.io/static/js/main.50cf4f6b.chunk.js"
+  },
+  {
+    "revision": "28833ad645419bc479ea",
+    "url": "/jfc4.github.io/static/js/runtime-main.64ad99ce.js"
+  }
+]);
